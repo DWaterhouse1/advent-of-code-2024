804eaf2e657291f176c7095bc173464a821a945a
Dan Waterhouse <d.waterhouse0496@gmail.com>
2024-12-04 08:56:00 +0000

    Day one, part two implementation

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,21 +1,27 @@
-use std::str;
+use std::{collections::HashMap, str};
 
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse_and_unzip(input: &str) -> Option<(Vec<i64>, Vec<i64>)> {
     const ROW_DELIMITER: &str = "\n";
     const PAIR_DELIMITER: &str = "   ";
 
-    let (mut lhs, mut rhs): (Vec<i64>, Vec<i64>) = input
-        .split(ROW_DELIMITER)
-        .take_while(|x| !x.is_empty())
-        .map(|x| {
-            let pair = x.split_once(PAIR_DELIMITER)?;
-            Some((pair.0.parse::<i64>().ok()?, pair.1.parse::<i64>().ok()?))
-        })
-        .collect::<Option<Vec<_>>>()?
-        .into_iter()
-        .unzip();
+    Some(
+        input
+            .split(ROW_DELIMITER)
+            .take_while(|x| !x.is_empty())
+            .map(|x| {
+                let pair = x.split_once(PAIR_DELIMITER)?;
+                Some((pair.0.parse::<i64>().ok()?, pair.1.parse::<i64>().ok()?))
+            })
+            .collect::<Option<Vec<_>>>()?
+            .into_iter()
+            .unzip(),
+    )
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut lhs, mut rhs) = parse_and_unzip(input)?;
 
     lhs.sort();
     rhs.sort();
@@ -30,7 +36,25 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (lhs, rhs) = parse_and_unzip(input)?;
+
+    let mut id_counts = HashMap::<i64, u32>::new();
+
+    for x in rhs {
+        match id_counts.get(&x) {
+            Some(count) => {
+                id_counts.insert(x, count + 1);
+            }
+            None => {
+                id_counts.insert(x, 1);
+            }
+        }
+    }
+
+    Some(lhs.into_iter().fold(0, |acc, x| match id_counts.get(&x) {
+        Some(count) => acc + (x as u32) * count,
+        None => acc,
+    }))
 }
 
 #[cfg(test)]
@@ -46,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
